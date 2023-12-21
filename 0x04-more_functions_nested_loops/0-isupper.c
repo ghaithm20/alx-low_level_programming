47259ab9e656da52ed1e0af011f5433edef61624
ghaithm20 <ghaithm20@gmail.com>
2023-12-21 13:08:14 -0800

    update 0-isupeer.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _isupper-uppercase letter
+ * _isupper-uppercase letters
  * @c:char to check
  *
  * Return 0 or 1
  */
-int _isupper(int c){
+int _isupper(int c)
+{	
 	if(c>='A' && c<='Z')
 		return 1;
 	else 
